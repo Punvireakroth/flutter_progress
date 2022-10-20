ec1f7f922897435612cf2d368c93caffe186228d
Punvireakroth <vireakrothpun@gmail.com>
2022-10-20 20:43:41 +0700

    initial commit

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Questions"),
+        ),
+        body: Column(children: [
+          Text("Answer these question"),
+          ElevatedButton(onPressed: null, child: Text("Click here")),
+          ElevatedButton(onPressed: null, child: Text("Click here")),
+          ElevatedButton(onPressed: null, child: Text("Click here")),
+        ]),
+      ),
+    );
+  }
+}
