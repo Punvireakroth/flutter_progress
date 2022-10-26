2eb5744d47c9236ff4cf9afb7c417e9aa7b79e76
Punvireakroth <vireakrothpun@gmail.com>
2022-10-27 01:00:21 +0700

    initial commit

---
+++ my_card/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text("Hello"),
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.only(left: 200),
+            padding: EdgeInsets.all(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
