d51739c91f357759018ad4fc6a91aa2c9700030c
Punvireakroth <vireakrothpun@gmail.com>
2022-12-07 11:30:26 +0700

    boxDecoration and tranform

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,35 +5,48 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) :super(key: key);
+  const Home({Key? key}) : super(key: key);
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) :super (key: key);
-
+  const HomeScreen({Key? key}) : super(key: key);
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: Text("Hello VireakRoth"),
       ),
       body: Center(
         child: Container(
           alignment: Alignment.center,
           width: 300,
-          height: 200,
+          height: 150,
           decoration: BoxDecoration(
-            color: Colors.yellow,
-            borderRadius: BorderRadius.circular(30)
+            color: Colors.red,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            border: Border.all(color: Colors.redAccent),
+            boxShadow: [
+              BoxShadow(color: Colors.grey.withOpacity(0.9), offset: Offset(4, 4)),
+              BoxShadow(color: Colors.yellow.withOpacity(0.7), offset: Offset(5, 5))
+            ]
           ),
-          child: Text("Hello World", style: TextStyle(fontSize: 40, color: Colors.white),),
+          transform: Matrix4.rotationZ(0.2),
+          child: Text(
+            "VireakRoth",
+            style: TextStyle(fontSize: 40, color: Colors.white),
           ),
+        ),
       ),
     );
   }
